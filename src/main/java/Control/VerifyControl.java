509c5a4d7f77d78e31504fb81eb7e94bd278a9d8
huynhquoi <115452240+huynhquoi@users.noreply.github.com>
2022-11-28 12:30:02 +0700

    Update

--- src/main/java/Control/VerifyControl.java
+++ src/main/java/Control/VerifyControl.java
@@ -29,8 +29,7 @@ public class VerifyControl extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        doPost(request, response);
+    	request.getRequestDispatcher("/shop/verify.jsp").forward(request, response);
     }
 
     /**
